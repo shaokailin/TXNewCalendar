98313f764d24dc514df33000ef3b76622957e76a
linshaokai <397002535@qq.com>
2018-05-03 14:37:48 +0800

    1111

--- TXBZSGAPP/Pods/Target Support Files/Pods-TXBZSGAPP/Pods-TXBZSGAPP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TXBZSGAPP : NSObject
-@end
-@implementation PodsDummy_Pods_TXBZSGAPP
-@end
--- TXTJSMAPP/Pods/Target Support Files/Pods-TXTJSMAPP/Pods-TXTJSMAPP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TXTJSMAPP : NSObject
-@end
-@implementation PodsDummy_Pods_TXTJSMAPP
-@end
